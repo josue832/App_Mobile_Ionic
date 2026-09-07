274a43530d39fdb672e3ebbf2bf872d0d25b9142
josue832 <diazjosue5637@gmail.com>
2026-09-06 18:19:56 -0600

    Login page con animacion SVG y anime.js v4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { animate } from 'animejs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  @ViewChild('animatedPath') pathRef!: ElementRef<SVGPathElement>;
+
+  private current: any = null;
+
+  private animatePath(strokeDashoffset: number, strokeDasharray: string): void {
+    if (this.current) {
+      this.current.pause();
+    }
+    this.current = animate(this.pathRef.nativeElement, {
+      strokeDashoffset: {
+        to: strokeDashoffset,
+        duration: 700,
+        ease: 'outQuart',
+      },
+      strokeDasharray: {
+        to: strokeDasharray,
+        duration: 700,
+        ease: 'outQuart',
+      },
+    });
+  }
+
+  onFocusEmail(): void {
+    this.animatePath(0, '240 1386');
+  }
+
+  onFocusPassword(): void {
+    this.animatePath(-336, '240 1386');
+  }
+
+  onFocusSubmit(): void {
+    this.animatePath(-730, '530 1386');
+  }
 }
